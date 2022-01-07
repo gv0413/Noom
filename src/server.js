4a9a1d4436a2802cf23afc18598fa0db27fe7171
gv0413 <ggvv0413@gmail.com>
2022-01-07 13:38:04 +0900

    import socketIO

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import WebSocket from "ws";
+import SocketIO from "socket.io"
 import http from "http";
 
 const app = express();
@@ -12,37 +12,34 @@ console.log("hello")
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 // http server
-const server = http.createServer(app)
-
-// ws server
-// http 서버 위에 ws 서버를 만들도록 하기 위해 server를 만들었고, 해당 객체에 접근할 수 있도록 함
-const wss = new WebSocket.Server({server})
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
 const sockets = [];
 
 // socket은 연결된 어떤 사람. 여결된 브라우저와의 컨택트
-wss.on("connection", (socket) =>{
-  sockets.push(socket);
-  socket["nickname"] = "Anon"
-  console.log("Connected to Browser")
-  // 브라우저 창이 닫히면 연결이 끊기면서 해당 콘솔로그 찍힘
-  socket.on("close", () => {
-    console.log("Disconnected from the Browser")
-  })
-  // 브라우저로부터 메세지를 받으면 콘솔로그
-  socket.on("message", msg => {
-    const message = JSON.parse(msg.toString('utf8'));
-    switch(message.type) {
-      case "new_message":
-        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`))
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
-    // 다른 소켓들에게 메세지를 보내준다
-    // sendback
-    // socket.send(message.toString('utf8'))
-  })
-  socket.send("hello!!!");
-})
+// wss.on("connection", (socket) =>{
+//   sockets.push(socket);
+//   socket["nickname"] = "cAnon"
+//   console.log("Connected to Browser")
+//   // 브라우저 창이 닫히면 연결이 끊기면서 해당 콘솔로그 찍힘
+//   socket.on("close", () => {
+//     console.log("Disconnected from the Browser")
+//   })
+//   // 브라우저로부터 메세지를 받으면 콘솔로그
+//   socket.on("message", msg => {
+//     const message = JSON.parse(msg.toString('utf8'));
+//     switch(message.type) {
+//       case "new_message":
+//         sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`))
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+//     // 다른 소켓들에게 메세지를 보내준다
+//     // sendback
+//     // socket.send(message.toString('utf8'))
+//   })
+//   socket.send("hello!!!");
+// })
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
