8da48b537ca23946dd84b5bb5792e0c16d5eaa70
gv0413 <ggvv0413@gmail.com>
2022-01-13 13:58:37 +0900

    someone room join -> welcome msg

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -8,6 +8,12 @@ room.hidden = true;
 
 let roomName;
 
+function addMessage(message) {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = message;
+  ul.appendChild(li);
+}
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
@@ -24,3 +30,7 @@ function handleRoomSubmit(event) {
   input.value = "";
 }
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", ()=> {
+  addMessage("someone joined!");
+})
